8b5dd56731015912d3a7cc44d4f776d19d98ea72
RobSimental <robertsimental@gmail.com>
2022-06-02 14:15:56 -0700

    adding folders

--- app.js
+++ app.js
@@ -11,11 +11,41 @@ app.get("/", (req, res) => {
 })
 
 app.get("/register", (req, res) => {
-    res.render("register");
+    res.render("account/register");
 })
 app.get("/forgotPassword", (req, res) => {
-    res.render("forgotPassword");
+    res.render("account/forgotPassword");
 })
+app.get("/profile", (req, res) => {
+    res.render("account/myProfile");
+})
+app.get("/events", (req, res) => {
+    res.render("events/events");
+})
+app.get("/event/:eventTitle", (req, res) => {
+    console.log(req.params);
+    res.render("events/eventProfile");
+})
+app.get("/create", (req, res) => {
+    res.render("events/createEvent");
+})
+app.get("/match", (req, res) => {
+    res.render("match/match")
+})
+app.get("/support", (req, res) => {
+    res.render("other/support")
+})
+app.get("/tnc", (req, res) => {
+    res.render("other/tnc")
+})
+app.get("/404", (req, res) => {
+    res.render("other/underConstruction")
+})
+
+
+
+
+
 app.listen(3000, () => {
     console.log("Started Server on Port 3000");
 })
